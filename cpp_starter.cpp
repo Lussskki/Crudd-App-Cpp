b11925163c96fa3bda0d682e4832f3e54a844810
Luka <monster.striekr21@gmail.com>
2025-02-08 11:05:21 +0400

    initial commit, starting crud app

--- cpp_starter.cpp
+++ cpp_starter.cpp
@@ -3,11 +3,5 @@
 using namespace std;
 
 int main() {
-    fstream myFile;
-    myFile.open("data.txt", ios :: out); // write or if doesn't exists it creates
-    if(myFile.is_open()) {
-        myFile << "It's working \n";
-        myFile.close();
-    }
-    system("pause");
+    
 }
