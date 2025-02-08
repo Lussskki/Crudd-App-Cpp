f86afd2fa4b35718e98c116c4d69a792f21548a2
Luka <monster.striekr21@gmail.com>
2025-02-08 11:55:51 +0400

    Feat: create struct and void

--- cpp_starter.cpp
+++ cpp_starter.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+
 using namespace std;
+
+struct Record {
+    int id;
+    string name;
+};
+
+void createRecord (int id, string name) {
+
+};
 
 int main() {
     
