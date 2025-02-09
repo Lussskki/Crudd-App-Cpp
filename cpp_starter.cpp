3abb8a388edfb43a8d28e6c82a81bb097532296f
Luka Guledani <lukaguledani123@gmail.com>
2025-02-09 13:06:01 +0400

    Feat, Refactor: add readability, fixbugs and simplified code

--- cpp_starter.cpp
+++ cpp_starter.cpp
@@ -6,14 +6,15 @@
 using namespace std;
 
 struct Record {
-    int choice, id;
-    string name;
+    int choice;
+    string data;
 };
 
-void createRecord (int id, string name) {
+// creating a file 
+void createRecord (string data) {
     ofstream file("data.txt", ios::app); // append mode
     if (file.is_open()){
-        file << " " << name << endl;
+        file << " " << data << endl;
         file.close();
         cout << "Record added successfully!\n";
     } else {
@@ -21,29 +22,52 @@ void createRecord (int id, string name) {
     }
 };
 
+// reading a file 
+void readRecords () {
+    ifstream file("data.txt");
+    if (file.is_open()) {
+        string data;
+        cout << "Data\n";
+        cout << "---------\n";
+        while (getline(file,data)) {
+            cout << data <<endl;
+        }
+        file.close();
+    } else {
+        cout << "Error opening file!\n";
+    }
+}
+
 int main() {
-    int choice, id;
-    string name;
+    int choice;
+    string data;
      
 
     do {
         cout << "\nCRUD operations:\n";
         cout << "1. Create Record.\n";
+        cout << "2. Read Record.\n";
+        cout << "3. Exit.\n";
         cout << "Enter your choice: "; 
         cin >> choice;
+        cin.ignore();
 
         switch (choice) {
             case 1:
-                cout << "Enter id: ";
-                cin >> id;
-
-                cout << "Enter name: ";
-                cin >> name;
+                cout << "Enter data: ";
+                getline(cin, data);
                   
-                createRecord(id, name);
-                break;    
+                createRecord(data);
+                break;
+            case 2:
+                readRecords();
+                break;
+            case 3:
+                   cout << "Exiting...\n";
+            default:
+                cout << "Bye...\n";        
         }
-    } while (choice != 1);
+    } while (choice != 3);
 
     return 0;   
 }
